8f15ac2903b09ee10a04b2c4751f8c983ae71115
phadatul <phadatul@gmail.com>
2020-12-17 12:28:59 +0530

    SPRING MVC

---
+++ DemoNewMVC/src/com/vwits/controllers/HelloWorld.java
@@ -0,0 +1,16 @@
+package com.vwits.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class HelloWorld {
+
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
+	public String helloMethod() {
+
+		return "welcome.jsp";
+	}
+
+}
